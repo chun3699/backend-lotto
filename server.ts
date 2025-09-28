b6e9dd7c0ffd3ee5b1f2d1a979c0c83ffe21fc40
chun3699 <162800403+chun3699@users.noreply.github.com>
2025-09-29 03:52:40 +0700

    แก้ไ1

--- server.ts
+++ server.ts
@@ -1,7 +1,9 @@
 import http from "http";
 import { app } from "./app";
 
-const port = process.env.port || 3000;
+// ใช้ process.env.PORT ตาม Vercel แนะนำ
+const port = process.env.PORT || 3000;
+
 const server = http.createServer(app);
 
 server.listen(port, () => {
